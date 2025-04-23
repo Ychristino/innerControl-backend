d2e22046ed65e2a963fc6bd398b57f66e3b7c48b
Ychristino <ychristino@gmail.com>
2025-04-23 17:41:26 -0300

    allow cords

--- src/main/java/com/innerControl/innerControl/config/security/CorsConfig.java
+++ src/main/java/com/innerControl/innerControl/config/security/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type", "content-type"));
